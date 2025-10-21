52e1dd9d9bbd86d2f508834b8dfedaee05cf5ef3
Ksenia <trifonovakseniia@gmail.com>
2025-10-21 10:13:51 +0300

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.example
+
+//TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
+// then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
+fun main() {
+
+}
