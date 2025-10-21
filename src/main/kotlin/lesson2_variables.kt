264f8dc74294b219ecdf26e17b43b9180f95bb52
Ksenia <trifonovakseniia@gmail.com>
2025-10-21 23:27:46 +0300

    KSL-2-1 variables

---
+++ src/main/kotlin/lesson2_variables.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main() {
+    var numberOfOrders: Int = 75
+    val welcomeMessage: String = "Благодарим за покупку!"
+
+    println(numberOfOrders)
+    println(welcomeMessage)
+}
